3020cf2c195969207a90424fe671b1a0b302f52e
xuyafei9303 <1007386723@qq.com>
2019-04-02 18:17:19 +0800

    test controller

---
+++ DYZB/DYZB/Classes/Home/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  DYZB
+//
+//  Created by root-x on 2019/4/2.
+//  Copyright © 2019 xuyafei. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
